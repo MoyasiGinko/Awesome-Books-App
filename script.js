ca9ddd6569667c9e5e83c545aeabfdfa4cc7c577
juandapps <juandapps@hotmail.com>
2023-05-02 14:09:42 -0500

    Fix book objects storage

--- script.js
+++ script.js
@@ -2,43 +2,39 @@
 const root = document.querySelector("#main");
 
 root.innerHTML = `
-        <form id="form-container">
-            <div class="form-items">
-                <label for="title">Title</label>
-                <input type="text" id="title" class="form-control">
-            </div>
-            <div class="form-items">
-                <label for="author">Author</label>
-                <input type="text" id="author" class="form-control">
-            </div>
-            <button type="submit" id="add-book">SUBMIT</button>
-        </form>
+    <form id="form-container">
+        <div class="form-items">
+            <label for="title">Title</label>
+            <input type="text" id="title" class="form-control">
+        </div>
+        <div class="form-items">
+            <label for="author">Author</label>
+            <input type="text" id="author" class="form-control">
+        </div>
+        <button type="submit" id="add-book">SUBMIT</button>
+    </form>
 `;
 
 // STORE BOOKS
 const library = [];
 
 // ADD BOOK
-
 const addBook = () => {
     const form = document.querySelector('form');
-    const addBookBtn = document.getElementById("add-book");
 
-  // STORE DATA
-  const { title, author } = form.elements;
+    // STORE DATA
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-    const storeData = (obj) => {
-        form.addEventListener('input', () => {
-            obj = {
-                title: title.value,
-                author: author.value
-            }
-            library.push(obj);
-        })
-    }
+        const { title, author } = form.elements;
+        const book = {
+            title: title.value,
+            author: author.value
+        };
 
-  // STORE BOOK TO LIBRARY
-  addBookBtn.addEventListener("click", storeData);
+        library.push(book);
+        console.log(library);
+    });
 };
 
 addBook();
