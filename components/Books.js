d8fde0234b948c1e1d86945c3bf1091923691929
Moyasi Ginko <mrsiyam61@gmail.com>
2023-05-04 01:49:55 +0600

    Add class methods addBook,deleteBook,saveLibrary

--- components/Books.js
+++ components/Books.js
@@ -12,11 +12,51 @@ class Books {
     this.saveLibrary();
   }
 
-  addBook = () => {};
+  addBook = () => {
+    const form = document.querySelector("#form-container");
+    
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
 
-  deleteBook = () => {};
+      const title = document.querySelector("#title");
+      const author = document.querySelector("#author");
 
-  saveLibrary = () => {};
+      if (title.value.trim() === "" || author.value.trim() === "") {
+        alert("Please enter a title and author");
+        return;
+      }
+
+      const book = {
+        title: title.value,
+        author: author.value,
+      };
+
+      this.#library.push(book);
+      title.value = "";
+      author.value = "";
+
+      this.render();
+    });
+  };
+
+  deleteBook = () => {
+    const libraryContainer = document.getElementById("library-container");
+
+    libraryContainer.addEventListener("click", (event) => {
+      if (event.target.id === "remove-book") {
+        const bookIndex = event.target.dataset.index;
+        this.#library.splice(bookIndex, 1);
+
+        this.render();
+      }
+    });
+  };
+
+  saveLibrary = () => {
+    window.addEventListener("beforeunload", () => {
+      localStorage.setItem("library", JSON.stringify(this.#library));
+    });
+  };
 }
 
 export default Books;
